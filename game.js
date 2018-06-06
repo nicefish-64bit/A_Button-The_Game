c74f7ca57a06c4d7a1446a3efc54d449a8ee0af4
rhiannon <rhiannon@localhost>
2018-06-06 20:08:49 +0000

    We are all connected

--- game.js
+++ game.js
@@ -1,11 +1,64 @@
-function game()
+let currentFn;
+
+function insertDivWithChildren(children)
 {
-    var g = document.getElementById('game');
+    var game = document.getElementById('game');
+
+    var div = document.createElement('div');
+    for (child of children) {
+        div.appendChild(child);
+    }
+
+    if (game.hasChildNodes()) {
+        game.insertBefore(div, game.firstChild);
+    } else {
+        game.appendChild(div);
+    }
+}
+
+function stage0()
+{
     var b = document.createElement('input');
     b.type = 'button';
     b.value = "A Button";
+    b.derpstate = 1;
     b.onclick = function () {
-        this.value = "Am clicked";
+        switch (this.derpstate) {
+        case 0:
+            this.value = "Am clicked";
+            this.derpstate++;
+            break;
+        case 1:
+            this.value = "Feed me";
+            this.derpstate++;
+            break;
+        case 2:
+            this.value = "Please?";
+            this.derpstate++;
+            stage1((msg) => {this.value = msg;});
+            break;
+        default:
+        }
     };
-    g.appendChild(b);
+
+    insertDivWithChildren([b]);
+}
+
+function stage1(messageWriterFn)
+{
+    var b = document.createElement('input');
+    b.type = 'button';
+    b.value = "Hi Again";
+    b.onclick = function () {
+        messageWriterFn("We are all connected");
+    };
+
+    insertDivWithChildren([b]);
+}
+
+currentFn = stage0;
+
+function game()
+{
+    currentFn();
 }
